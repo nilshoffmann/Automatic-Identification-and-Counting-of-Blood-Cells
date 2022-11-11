27b0750fdd0a76716db47c474611021eb1b1b676
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2022-11-11 17:38:22 +0100

    Added ONNX Readme and Conda environment.

---
+++ README-ONNX.md
@@ -0,0 +1,32 @@
+# Running the RBC Model with ONNX Runtime
+
+## Installing the runtime environment with Conda
+
+The `environment.txt` contains the libraries that were installed using Conda. 
+To recreate the environment on your system, please install Conda / MiniConda first.
+
+Then run:
+
+  conda env create -f environment.yml
+
+and
+
+  conda activate RBCs
+
+to activate the environment. This should install all necessary dependencies independently of your local Python installation and packages.
+
+## Running the code
+
+Make sure that the RBCs Conda environment is activated:
+
+  conda activate RBCs
+
+Then run:
+
+  python3 detect-and-predict.onnx
+
+This will first run the tensorflow model for RBCs and write out the model graph with weights.
+In turn, the serialized model will be parsed and converted to ONNX format.
+Thereafter, the ONNX model will be loaded and an inference session is created and executed with ONNX runtime.
+
+Result output for both models on the test image01 are written to serialized numpy arrays as 'yolov2-predictions.npy' and 'onnx-predictions.npy' respectively.
--- detect-and-predict-onnx.py
+++ detect-and-predict-onnx.py
@@ -1,18 +1,113 @@
-import os
-
+import time
+from utils import iou
+from scipy import spatial
+from darkflow.net.build import TFNet
 import cv2
 import numpy as np
 import tensorflow as tf
 import onnx
 import tf2onnx
 import onnxruntime as rt
-from onnx import helper, shape_inference
-from onnx import TensorProto
-
-from darkflow.net import yolov2
-from darkflow.net.yolo.predict import _fix
 from darkflow.utils.im_transform import imcv2_affine_trans, imcv2_recolor
 
+
+options = {'model': 'cfg/tiny-yolo-voc-3c.cfg',
+           'load': 3750,
+           'threshold': 0.1,
+           'gpu': 0.7}
+
+tfnet = TFNet(options)
+
+pred_bb = []  # predicted bounding box
+pred_cls = []  # predicted class
+pred_conf = []  # predicted class confidence
+
+
+def blood_cell_count(file_name):
+    rbc = 0
+    wbc = 0
+    platelets = 0
+
+    cell = []
+    cls = []
+    conf = []
+
+    record = []
+    tl_ = []
+    br_ = []
+    iou_ = []
+    iou_value = 0
+
+    tic = time.time()
+    image = cv2.imread('data/' + file_name)
+    output = tfnet.return_predict(image)
+    print("Saving TF Model Predictions in 'yolov2-predictions.npy'")
+
+    for prediction in output:
+        label = prediction['label']
+        confidence = prediction['confidence']
+        tl = (prediction['topleft']['x'], prediction['topleft']['y'])
+        br = (prediction['bottomright']['x'], prediction['bottomright']['y'])
+
+        if label == 'RBC' and confidence < .5:
+            continue
+        if label == 'WBC' and confidence < .25:
+            continue
+        if label == 'Platelets' and confidence < .25:
+            continue
+
+        # clearing up overlapped same platelets
+        if label == 'Platelets':
+            if record:
+                tree = spatial.cKDTree(record)
+                index = tree.query(tl)[1]
+                iou_value = iou(tl + br, tl_[index] + br_[index])
+                iou_.append(iou_value)
+
+            if iou_value > 0.1:
+                continue
+
+            record.append(tl)
+            tl_.append(tl)
+            br_.append(br)
+
+        center_x = int((tl[0] + br[0]) / 2)
+        center_y = int((tl[1] + br[1]) / 2)
+        center = (center_x, center_y)
+
+        if label == 'RBC':
+            color = (255, 0, 0)
+            rbc = rbc + 1
+        if label == 'WBC':
+            color = (0, 255, 0)
+            wbc = wbc + 1
+        if label == 'Platelets':
+            color = (0, 0, 255)
+            platelets = platelets + 1
+
+        radius = int((br[0] - tl[0]) / 2)
+        image = cv2.circle(image, center, radius, color, 2)
+        font = cv2.FONT_HERSHEY_COMPLEX
+        image = cv2.putText(image, label, (center_x - 15, center_y + 5), font, .5, color, 1)
+        cell.append([tl[0], tl[1], br[0], br[1]])
+
+        if label == 'RBC':
+            cls.append(0)
+        if label == 'WBC':
+            cls.append(1)
+        if label == 'Platelets':
+            cls.append(2)
+
+        conf.append(confidence)
+
+    toc = time.time()
+    pred_bb.append(cell)
+    pred_cls.append(cls)
+    pred_conf.append(conf)
+    avg_time = (toc - tic) * 1000
+    print('{0:.5}'.format(avg_time), 'ms')
+
+    cv2.imwrite('output/' + file_name, image)
 
 def analyze_inputs_outputs(graph):
     ops = graph.get_operations()
@@ -34,9 +129,13 @@ def resize_input(im, meta):
     imsz = imsz[:, :, ::-1]
     return imsz
 
+image_name = 'image_001.jpg'
+blood_cell_count(image_name)
+print('Saving TF model!')
+tfnet.savepb()
 
 name = 'built_graph/tiny-yolo-voc-3c.pb'
-
+print(f"Parsing Model {name}")
 graph_def = tf.compat.v1.GraphDef()
 with open(name, 'rb') as f:
     graph_def.ParseFromString(f.read())
@@ -78,7 +177,7 @@ except onnx.checker.ValidationError as e:
 else:
     print("The model is valid!")
 
-print(onnx.helper.printable_graph(onnx_model.graph))
+#print(onnx.helper.printable_graph(onnx_model.graph))
 
 # read test image
 print("Reading test image")
@@ -102,7 +201,7 @@ for t in sess.get_inputs():
 
 for t in sess.get_outputs():
     print("Session output:", t.name, t.type, t.shape)
-print("Model Predictions for test image:")
+print("Saving ONNX Model Predictions in 'onnx-predictions.npy'")
 #print(outputs[0].shape, type(outputs))
 #print(pred_onnx)
 np.save('onnx-predictions', pred_onnx)
